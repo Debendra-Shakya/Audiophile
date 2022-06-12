83863c451943cf74aaa3e26a6690491ac4dca44e
debz1 <debenshakya1@gmail.com>
2022-06-12 19:42:56 +0545

    using useState to make dynamic (refresh not needed)

--- src/componenets/Home/Headphones/Headphone.js
+++ src/componenets/Home/Headphones/Headphone.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container, Section,MainHeading} from "../../../globalStyles";
+import { Button, Container, Section, MainHeading } from "../../../globalStyles";
 import {
   HeadphoneImage,
   HeadphoneSection,
@@ -11,46 +11,64 @@ import {
   Wrapper,
 } from "./HeadphoneStyles";
 
-
+import { useEffect, useState } from "react";
 const Headphone = () => {
+  const desktopImage =
+    process.env.PUBLIC_URL + "/assests/home/desktop/image-hero.jpg";
+  const tabletImage =
+    process.env.PUBLIC_URL + "/assests/home/tablet/image-header.jpg";
 
-  const desktopImage = process.env.PUBLIC_URL+ "/assests/home/desktop/image-hero.jpg";
-  const tabletImage = process.env.PUBLIC_URL + "/assests/home/tablet/image-header.jpg";
+  // const ImageUrl = window.innerWidth >= 943 ? desktopImage : tabletImage;
 
-  const ImageUrl = window.innerWidth >= 943 ? desktopImage : tabletImage;
-  console.log(window.innerWidth)
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  function getWindowSize() {
+    const { innerWidth } = window;
+    return { innerWidth };
+  }
+  const ImageUrl = windowSize.innerWidth >= 943 ? desktopImage : tabletImage;
+
   return (
-    <HeadphoneSection >
-          {/* <HeadphoneImage src="https://debz1.github.io/Audiophile/assests/image-hero.jpg" alt="hero"/> */}
-          <HeadphoneImage src={ImageUrl} alt="hero"/>
-    
- 
-      <hr style={{
+    <HeadphoneSection>
+      {/* <HeadphoneImage src="https://debz1.github.io/Audiophile/assests/image-hero.jpg" alt="hero"/> */}
+      <HeadphoneImage src={ImageUrl} alt="hero" />
+
+      <hr
+        style={{
           background: "white",
-          color: 'white',
-          borderColor: 'white',
-          height: '1px',
-          width: '100%',
-          
-          marginBottom:'60px',
-          marginTop:'-130px',
+          color: "white",
+          borderColor: "white",
+          height: "1px",
+          width: "100%",
+
+          marginBottom: "60px",
+          marginTop: "-130px",
           opacity: "0.3",
-  
-        
-      }}
-          />
+        }}
+      />
 
-
-
-   <Container>
+      <Container>
         <HeadphoneText>NEW PRODUCT</HeadphoneText>
         <HeadphoneMainHeading>XX99 MARK II HEADPHONES</HeadphoneMainHeading>
-        <HeadphoneTextOne>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</HeadphoneTextOne>
+        <HeadphoneTextOne>
+          Experience natural, lifelike audio and exceptional build quality made
+          for the passionate music enthusiast.
+        </HeadphoneTextOne>
         <ButtonWrapper>
           {/* <Button>get started</Button> */}
           <HeadphoneButton>SEE PRODUCT</HeadphoneButton>
         </ButtonWrapper>
-        </Container>
+      </Container>
     </HeadphoneSection>
   );
 };
