9fc82aa993b815ed97facaec33a79198e1b30fcc
debz1 <debenshakya1@gmail.com>
2022-06-23 19:03:31 +0545

    add to cart solved

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 const initialState = {
   cartItems: [],
@@ -6,14 +6,13 @@ const initialState = {
   total: 0,
 };
 
-export const addItemstoCart = (el, quantity) => async (dispatch) => {
+export const addItemstoCart = (el,quantity) => async (dispatch) => {
   // dispatch(AddToCart(el,quantity))
   dispatch(
     AddToCart({
-      payload: {
         cartItems: el,
-        quantity,
-      },
+        quantity:quantity
+    
     })
   );
 };
@@ -22,41 +21,52 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    // AddToCart: (state, action) => {
-    //  const itemIndex= state.cartItems.findIndex((item)=> item.id ===action.payload.id)
-    //  if(itemIndex >= 0){
-    //   state.cartItems[itemIndex].cartQuantity += 1
-    //  }else{
-
-    //    const tempProduct={...action.payload,cartQuantity:1}
-    //    state.cartItems.push(tempProduct)
-    //  }
-    // },
     AddToCart: (state, action) => {
-      const items = action.payload;
-      const item = items.payload;
-      console.log(item);
-      const isItemExist = state.cartItems.map(
-        (item) => item.id === item.cartItems.id
-      );
+      console.log(current (state))
+      console.log(action.payload)
+      // console.log(action.payload.payload.cartItems.id)
+     const itemIndex= state.cartItems.findIndex((item)=> item.id===action.payload.cartItems.id)
+
+     console.log(itemIndex)
+
+     //itemIndex is working fine
+     if(itemIndex >= 0){
+      state.cartItems[itemIndex].cartQuantity += action.payload.quantity
+      console.log(current(state.cartItems))
+     }else{
+
+       const tempProduct={...action.payload.cartItems,cartQuantity:action.payload.quantity}
+      //  state.cartItems.push(tempProduct)
+      console.log(tempProduct)
+       state.cartItems= [...state.cartItems, tempProduct]
+       console.log(current (state))
+     }
+    },
+    // AddToCart: (state, action) => {
+    //   const items = action.payload;
+    //   const item = items.payload;
+    //   console.log(item);
+    //   const isItemExist = state.cartItems.map(
+    //     (item) => item.id === item.cartItems.id
+    //   );
     
       
 
-      if (!isItemExist) {
-        state.cartItems = [
-          ...state.cartItems,
-          { ...item.cartItems, quantity: item.quantity },
-        ];
-      } else {
-        state.cartItems = state.cartItems.map((i) => {
-          if (i.id === item.cartItems.id) {
-            return { ...i, quantity: item.quantity + state.cartItems.quantity };
-          } else {
-            return i;
-          }
-        });
-      }
-    },
+    //   if (!isItemExist) {
+    //     state.cartItems = [
+    //       ...state.cartItems,
+    //       { ...item.cartItems, quantity: item.quantity },
+    //     ];
+    //   } else {
+    //     state.cartItems = state.cartItems.map((i) => {
+    //       if (i.id === item.cartItems.id) {
+    //         return { ...i, quantity: item.quantity + state.cartItems.quantity };
+    //       } else {
+    //         return i;
+    //       }
+    //     });
+    //   }
+    // },
  
   },
 });
